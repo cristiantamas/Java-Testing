56d2ae4f6202a5fe1a3bc90befc4736fb4a166b4
Cristian Tamas <cristiantamas05@gmail.com>
2018-05-07 20:44:23 +0300

    I don't understand what's going on

---
+++ src/test/java/java/TestApp.java
@@ -0,0 +1,21 @@
+package java;
+
+import org.junit.Test;
+
+public class TestApp {
+
+    @Test
+    public void testA(){
+        assert(true);
+    }
+
+    @Test
+    public void testB(){
+        assert(true);
+    }
+
+    @Test
+    public void testC(){
+        assert(true);
+    }
+}
