c2d14a83715e7e044003fa43f4eecb60fdf8bcf2
Cristian Tamas <cristiantamas05@gmail.com>
2018-05-07 20:22:29 +0300

    Added blank test cases Added JUnit dependency

---
+++ src/test/java/java/IncrementalIntegration.java
@@ -0,0 +1,22 @@
+package test.java.java;
+
+import org.junit.Test;
+
+public class IncrementalIntegration {
+
+
+    @Test
+    public void testA(){
+        assert(true);
+    }
+
+    @Test
+    public void testB(){
+        assert(true);
+    }
+
+    @Test
+    public void testC(){
+        assert(true);
+    }
+}
